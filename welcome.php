f547b15f2b4fe2c12b4108b9e17092a7073d589d
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-01-18 12:03:28 +0100

    big changes

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<html">
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome <?php echo $login_session; ?></h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
