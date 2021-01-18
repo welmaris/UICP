fbfa39d0ac926226d6d89595e9c14002e1f414f7
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-01-18 11:32:33 +0100

    test 3

--- inloggen.php
+++ inloggen.php
@@ -1,141 +1,66 @@
-<?php
-require("config.php");
-session_start();
-?>
-
 <html>
-<style>
-
-body {
-	background-color: white;
-}
-
-h1 {
-	text-align: center;
-	position: relative;
-	top: 30px;
-	border: 3px solid black;
-	
-}
-
-div {
-	text-align:right;
-	float: center;
-	width: 100px;
-	height: 100px;
-	
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-  	
-	margin: auto;
-}
-</style>
-
-<head>
-	<title>Login</title>
-</head>
-<body>
-<header>
-	<?php echo printheader("Login") ?>
-</header>
-
-<h1>Login</h1>
-
-<div>
-	<table>
-		<form action="" method="post" >
-			<tr><td><label for="email">Emailaddress:</label></td><td><input type="email" name="email" id="email" required></td></tr>
-			<tr><td><label for="password">Password: </label></td><td><input type="password" name="password" id="password" required></td></tr>
-			<tr><td></td><td><input type="submit" name="login" value="Log in"></td></tr>
-		</form>
-		<tr><td>Forgotten password? </td><td><a href="forgotten_password.php">Klik hier!</a></p></td></tr>
-	</table>
-</div>
-
+   <head>
+      <title>Login Page</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style> 
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+            <div style = "margin:30px">   
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>	
+            </div>	
+         </div>	
+      </div>
+   </body>
+</html>
 
 <?php
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_POST["loggedin"]) && $_POST["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
-}
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-    // Close connection
-    mysqli_close($link);
-}
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
 ?>
