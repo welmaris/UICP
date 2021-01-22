18c33e6c358a163100a14fb665d81d0cfa87db3f
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-01-22 11:52:25 +0100

    TEST 2 GRAFIEK

--- grafiek.php
+++ grafiek.php
@@ -41,9 +41,4 @@
     }
     </script>
     </head>
-    <body>
-    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
-    <script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
-    <script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
-    </body>
     </html>                              
