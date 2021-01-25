cc2b0ff5cf5a1e0efa89f8dec87522a2926844d5
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-01-25 10:21:13 +0100

    opening json

--- grafiek.php
+++ grafiek.php
@@ -1,44 +1,6 @@
-<!DOCTYPE HTML>
-    <html>
-    <head>
-    <script>
-    window.onload = function() {
-     
-    var dataPoints = [];
-     
-    var chart = new CanvasJS.Chart("chartContainer", {
-    	animationEnabled: true,
-    	theme: "light2",
-    	zoomEnabled: true,
-    	title: {
-    		text: "Bitcoin Price - 2017"
-    	},
-    	axisY: {
-    		title: "Price in USD",
-    		titleFontSize: 24,
-    		prefix: "$"
-    	},
-    	data: [{
-    		type: "line",
-    		yValueFormatString: "$#,##0.00",
-    		dataPoints: dataPoints
-    	}]
-    });
-     
-    function addData(data) {
-    	var dps = data.price_usd;
-    	for (var i = 0; i < dps.length; i++) {
-    		dataPoints.push({
-    			x: new Date(dps[i][0]),
-    			y: dps[i][1]
-    		});
-    	}
-    	chart.render();
-    }
-     
-    $.getJSON("https://canvasjs.com/data/gallery/php/bitcoin-price.json", addData);
-     
-    }
-    </script>
-    </head>
-    </html>                              
+<?php
+// Get the contents of the JSON file 
+$strJsonFileContents = file_get_contents("/../data.json");
+var_dump($strJsonFileContents); // show contents
+
+?>
