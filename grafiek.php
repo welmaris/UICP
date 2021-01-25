1d2f72540e787c09e7ab1c53705238c5f20a616c
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-01-25 10:22:47 +0100

    opening json 2

--- grafiek.php
+++ grafiek.php
@@ -1,6 +1,6 @@
 <?php
 // Get the contents of the JSON file 
-$strJsonFileContents = file_get_contents("/../data.json");
+$strJsonFileContents = file_get_contents("/../../data.json");
 var_dump($strJsonFileContents); // show contents
 
 ?>
