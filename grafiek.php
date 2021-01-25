f202c69c94296e8617471566916c94a024823873
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2021-01-25 12:19:50 +0100

    Creating array from jason file

--- grafiek.php
+++ grafiek.php
@@ -5,6 +5,13 @@ $jsonfile = file_get_contents("../data.json");
 
 //json to array
 $array = json_decode($jsonfile, true);
-print_r($array[0]["TEMP"]);
+echo '<pre>'; print_r($array); echo '</pre>'; //print $array
 
-?>
+//looping trough array and making a new one with relevant data
+foreach($array as $value)
+{
+    $result[]=array($value["DATE"],$value["TIME"], $value["TEMP"], $value["PRCP"]);
+}
+//echo '<pre>'; print_r($result); echo '</pre>'; //print $result
+
+?>                                                 
